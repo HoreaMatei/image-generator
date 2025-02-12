1b877c04a379d7279ca368666533441fd68bf5c0
HoreaMatei <mateihorea17@yahoo.com>
2025-02-12 14:19:05 +0200

    changes to frontend

--- frontend/src/store/auth-context.jsx
+++ frontend/src/store/auth-context.jsx
@@ -1,6 +1,6 @@
 import { createContext, useState, useContext, useEffect } from "react";
 
-const backendPORT = "http://localhost:3000";
+const backendPORT = "https://image-generator-ukw2.vercel.app";
 
 const AuthContext = createContext({
   token: null,
