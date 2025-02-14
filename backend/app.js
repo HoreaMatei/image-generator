aa5df17806dbe9a5698a475a6346328a0124a643
HoreaMatei <mateihorea17@yahoo.com>
2025-02-14 11:48:38 +0200

    port changes for deploy

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "https://image-generator-3h9jdsadas.vercel.app", // Your frontend URL
+    origin: "*", // Your frontend URL
     credentials: true, // Allow cookies and authorization headers
   })
 );
