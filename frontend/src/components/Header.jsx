4f8d4f3a48498640e67f651ee8210f71167e6976
HoreaMatei <mateihorea17@yahoo.com>
2025-04-07 11:05:31 +0300

    header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <header className="relative text-center text-stone-50  mt-24 sm:mt-20 lg:mt-48 ">
+    <header className="relative text-center text-stone-50  mt-24 sm:mt-20 lg:mt-[20vh] ">
       <h1 className="text-2xl  sm:text-3xl lg:text-5xl font-bold font-mono z-10">
         AI Image Generator
       </h1>
